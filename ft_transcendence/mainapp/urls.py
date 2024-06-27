4a7bd8164a6478faaa01fb0155547aa3beaeeeb7
BrunoB <BrunoPierreBouchard@hotmail.ca>
2024-06-27 03:42:28 -0400

    added logout

--- ft_transcendence/mainapp/urls.py
+++ ft_transcendence/mainapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="accueil"),
     path("login_check", views.login_check),
+    path("logout_check", views.logout_check, name = "logout"),
     path("register", views.register, name="register")
 ]
--- ft_transcendence/mainapp/views.py
+++ ft_transcendence/mainapp/views.py
@@ -2,14 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.urls import reverse
 from mainapp.models import CustomUser
 
 # Create your views here.
-def index(request):    
-    return render(request, "ft_transcendence.html")
+def index(request):
+    return render(request, "ft_transcendence.html", {
+        "user":request.user
+    })
 
 def login_check(request):
     if request.method == "POST":
@@ -18,12 +20,16 @@ def login_check(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return JsonResponse({'username': user.get_username()})
+            return redirect(reverse("accueil"))
+            #return JsonResponse({'username': user.get_username()})
         else:
             return JsonResponse({'error': "Login or password incorrect"})
     else:
         return redirect(reverse("accueil"))
-
+    
+def logout_check(request):
+    logout(request)
+    return redirect(reverse("accueil"))
 def register(request):
     if request.method == "POST":
         username = request.POST['username']
@@ -38,6 +44,7 @@ def register(request):
 
             new_user = CustomUser.objects.create_user(username=username, password=password)
             new_user.save()
+            login(request, new_user)
             return redirect(reverse("accueil"))
     else:
         form = UserCreationForm()
