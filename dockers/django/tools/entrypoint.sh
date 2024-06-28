197ba53f5373df50985ad35a4b7e6543b90b0083
Shroomy Squid <f.a.r.barrette@hotmail.com>
2024-06-28 19:37:07 -0400

    merged PO dockers with main

---
+++ dockers/django/tools/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# while true
+# 	do echo "Bash running"
+# done
+
+# ls
+# cd ../../..
+# echo "after cd ../../.."
+# ls
+# echo "find ../../.. : "
+# find ../../.. -name "ft_transcendence"
+# echo "find ../.. : "
+# find ../.. -name "ft_transcendence"
+# echo "find ../ : "
+# find ../ -name "ft_transcendence"
+# echo "find : "
+# find -name "ft_transcendence"
+# echo "find done."
+
+
+exec $@
